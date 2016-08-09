9c1422adda2a65e3eaee879e5005c949237d6fb9
fwb7014 <258982902@qq.com>
2016-08-10 00:02:34 +0800

    20160809晚提交

---
+++ src/main/java/com/fwb/work/MyJob.java
@@ -0,0 +1,6 @@
+package com.fwb.work;
+/**
+ * Created by Administrator on 2016/8/9.
+ */
+public class MyJob  e{
+}
---
+++ src/test/java/test/MyTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2016/8/9.
+ */
+public class MyTest {
+
+    @Test
+    public void sayHi(){
+        System.out.println("11111111111111111111111111111111"+ new Date());
+
+    }
+}
